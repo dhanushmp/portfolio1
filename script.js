bd52e1a55c2cc093f9f50fdcd9754b63494187ea
iqq17 <iquasim05@gmail.com>
2024-08-01 22:03:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+
+var options = {
+  strings: ['Currently a software intern @ Headstarter AI', 'A Budding Full Stack Developer ',' An Aspiring ML Engineer'],
+  typeSpeed: 50,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true
+};
+
+var typed = new Typed('#element', options);
+
