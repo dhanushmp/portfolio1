6220addd8ca1b06fe49b22a6a21da44008a27195
ChickeNuggie <prisxjun123@gmail.com>
2024-10-10 22:12:09 +0800

    Update to personal portfolio to fill my details.

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Function to open the modal
+function openModal() {
+  document.getElementById("videoModal").style.display = "flex"; // Show the modal
+}
+
+// Function to close the modal
+function closeModal() {
+  document.getElementById("videoModal").style.display = "none"; // Hide the modal
+}
